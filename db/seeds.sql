97e17f617727e471686b4d52005ac6decc527108
Jane <janekhl@yahoo.com>
2018-06-25 21:43:02 -0700

    its working

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ("Double-Double");
+INSERT INTO burgers (burger_name) VALUES ("Big Mac");
+INSERT INTO burgers (burger_name) VALUES ("Hawaiian");
+INSERT INTO burgers (burger_name) VALUES ("Vegan Gluten Free");
+INSERT INTO burgers (burger_name) VALUES ("Turkey");
